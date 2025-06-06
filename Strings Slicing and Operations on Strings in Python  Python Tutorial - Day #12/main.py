a7e1ebd63068b8ca52718d0e9a5a1ab5b8d5dba6
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-06-06 22:08:56 +0530

    Update main.py

---
+++ Strings Slicing and Operations on Strings in Python  Python Tutorial - Day #12/main.py
@@ -0,0 +1,13 @@
+names = "Gajendra"
+
+# We can find the length of a string using len() function.
+
+print("Gajendra is a", len(names), "letter word")
+
+print(names[0:5]) #including 0 but not 8
+print(names[1:5])
+print(names[:5])
+print(names[:])
+print(names[0:-3])
+print(names[0:len(names)-3])
+print(names[-5:-3])
