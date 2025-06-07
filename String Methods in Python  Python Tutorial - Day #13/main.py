fc8f33a342e3a4278b15aad6913cec4f315435d9
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-06-07 16:54:34 +0530

    updated

---
+++ String Methods in Python  Python Tutorial - Day #13/main.py
@@ -0,0 +1,65 @@
+#Strings are immutable
+
+# name = "gajendra singh"
+# print(name.upper())
+
+# str1 = "GAJENDRA SINGH"
+# print(str1.lower())
+
+# str2 = "!!!Gajendra!!!!!!!"
+# print(str2.rstrip("!"))
+
+# str3 = "Gajendra"
+# print(str3.replace("Gajendra", "Manish"))
+
+# str4 = "Gajendra Singh Verma"
+# print(str4.split(" "))
+
+# blogHeading = "introduction to jS"
+# print(blogHeading.capitalize())
+
+# str5 = "welcome to the Js Chapter"
+# print(str5.center(50))
+
+# str6 = "abcdaeafgah"
+# print(str6.count("a"))
+
+# str7 = "welcome to the console !!!"
+# print(str7.endswith("!!"))
+
+# str8 = "He's name is Dan. He is an honest man."
+# print(str8.find("is"))
+
+# str9 = "He's name is Dan. Dan is an honest man."
+# print(str9.index("Dan"))
+
+# str10 = "WelcomeToTheConsole11"
+# print(str10.isalnum())
+
+# str11 = "Welcome"
+# print(str11.isalpha())
+
+# str12 = "hello world"
+# print(str12.islower())
+
+# str13 = "We wish you a Merry Christmas\n"
+# print(str13.isprintable())
+
+# str14 = "        "       #using Spacebar
+# print(str14.isspace())
+
+
+# str15 = "World Health Organization" 
+# print(str15.istitle())
+
+# str16 = "WORLD HEALTH ORGANIZATION" 
+# print(str16.isupper())
+
+# str17 = "Python is a Interpreted Language" 
+# print(str17.startswith("Python"))
+
+# str18 = "Python is a Interpreted Language" 
+# print(str18.swapcase())
+
+str19 = "He's name is Dan. Dan is an honest man."
+print(str19.title())
