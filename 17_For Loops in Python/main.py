4d32ed27178b6f9d7b10d1b44f50a6c20c071e3f
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-06-15 08:01:05 -0700

    ok

---
+++ 17_For Loops in Python/main.py
@@ -0,0 +1,19 @@
+# name = "Gajendra"
+# for i in name:
+#     print(i)
+
+# colors = ["red","blue", "white","black"]
+# for color in colors:
+#     print(color)
+
+    # for i in color:
+    #     print(i)
+
+# for k in range(5):
+#     print(k)
+
+# for k in range(5):
+#     print(k + 1)
+
+for k in range(1,12,3):
+    print(k)
