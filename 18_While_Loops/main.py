11e0114003d9aa54d2029acedfe673ca218868e3
Gajendra <gs55988@gmail.com>
2025-06-16 21:34:38 +0530

    Create main.py

---
+++ 18_While_Loops/main.py
@@ -0,0 +1,19 @@
+# i = 0
+# while(i < 30):
+#     print(i)
+#     i = i + 1
+
+# print("done with the loop")
+
+# i = int(input("enter the number"))
+# while(i < 30):
+#     i = int(input("enter the number"))
+#     print(i)
+    
+# print("done with the loop")
+
+# count = 5
+# while(count>0):
+#     print(count)
+#     count = count-1
+
