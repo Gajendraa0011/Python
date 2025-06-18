025274ec895d6a2da896c42ef98d7e10a67cfffb
Gajendra <gs55988@gmail.com>
2025-06-18 14:39:36 +0530

    Create main.py

---
+++ 19_break_and_continue/main.py
@@ -0,0 +1,23 @@
+# for i in range(6):
+#     if i==3:
+#       break
+#     print(i)
+
+# for i in range(6):
+#     if i==3:
+#       continue
+#     print(i)
+
+
+for i in range(12):
+    if i==10:
+        break
+    print("5 x", "i", "=", i*5)
+print("Loop has ended")
+
+for i in range(12):
+    if i==10:
+        print("skip the iteration")
+        continue
+    print("5 x", "i", "=", i*5)
+
