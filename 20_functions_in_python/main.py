990bb088dfa975e3fd7efcfd44bb696701d268e1
Gajendra <gs55988@gmail.com>
2025-06-22 12:26:18 +0530

    done

---
+++ 20_functions_in_python/main.py
@@ -0,0 +1,20 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater")
+
+    else:
+        print("Second number is greater")
+
+a = 44
+b = 55
+calculateGmean(a,b)
+isGreater(a,b)
+
+c = 440
+d = 55
+calculateGmean(c,d)
+isGreater(c,d)
